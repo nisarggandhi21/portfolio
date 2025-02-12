36bf3e028631016a8f3d7f03b9cb14ec9ac5b527
Nisarg Gandhi <nisarggandhi21@gmail.com>
2025-02-12 23:33:34 +0530

    disabled navbar

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,19 @@
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
-import { FloatingNav } from "@/components/ui/FloatingNav";
-import { FaHome } from "react-icons/fa";
-import { IoIosPerson } from "react-icons/io";
+// import { FloatingNav } from "@/components/ui/FloatingNav";
+// import { FaHome } from "react-icons/fa";
+// import { IoIosPerson } from "react-icons/io";
 
 export default function Home() {
   return (
     <div className="relative bg-black flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
-        <FloatingNav
+        {/* <FloatingNav
           navItems={[
             { name: "Home", link: "/", icon: <FaHome /> },
             { name: "About", link: "#about", icon: <IoIosPerson /> },
           ]}
-        />
+        /> */}
         <Hero />
         <Grid />
       </div>
